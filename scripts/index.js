ede31800bc8a4900c6e859fe64b8928ada22bf03
GRAPHISTE7 <graphiste7@iMac-Maya.local>
2019-01-31 12:18:08 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    slidesNavigation: true,
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
