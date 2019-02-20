fae52883ea04f7ffaa37ccdcc595e3bd6c5a9dfd
GRAPHISTE7 <graphiste7@iMac-Maya.local>
2019-02-20 16:26:59 +0100

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,21 @@
 new fullpage('#fullpage', {
-    //options here
-    autoScrolling:true,
-    scrollHorizontally: true,
-    navigation: true,
-    slidesNavigation: true,
+  //options here
+  autoScrolling:true,
+  scrollHorizontally: true,
+  slidesNavigation: true,
+  scrollingSpeed: 1000,
 });
 
 //methods
-fullpage_api.setAllowScrolling(true);
+// fullpage_api.setAllowScrolling(true);
+const top_scroll = document.getElementById('top');
+const down_scroll = document.getElementById('down');
+
+top_scroll.addEventListener('click', function () {
+  fullpage_api.moveSectionUp();  
+});
+
+down_scroll.addEventListener('click', function () {
+  fullpage_api.moveSectionDown();
+});
+
